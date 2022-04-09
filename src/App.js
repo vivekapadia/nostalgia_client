248d1fdc354703f65c712bcd7353aa5a4cf31060
vivekapadia <610vivek@gmail.com>
2022-04-09 19:07:09 +0530

    First look home page with Name Logo Form Posts and Post render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import nostalgia from "./images/nostalgia.png";
 
 const App = () => {
 	return (
-		<div>
-			<h1>APP</h1>
-		</div>
+		<Container maxWidth="lg">
+			<AppBar position="static" color="inherit">
+				<Typography variant="h2" align="center">
+					Nostalgia
+				</Typography>
+				<img src={nostalgia} alt="nostalgia_icon" height="60" />
+			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid
+						container
+						justify="space-between"
+						alignItems="stretch"
+						spacing={3}
+					>
+						<Grid item xs={12} sm={7}>
+							<Posts />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
 	);
 };
 
