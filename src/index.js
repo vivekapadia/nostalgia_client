077ea963304ee8d0d0dbb69e03ded140ef90c4b2
vivekapadia <610vivek@gmail.com>
2022-04-09 21:55:56 +0530

    import react-redux redux redux-thunk for intitialise redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./App.js";
 
