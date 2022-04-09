fedfd6a73055fcb7ae724fb822ea46e638838edd
vivekapadia <610vivek@gmail.com>
2022-04-09 19:39:41 +0530

    NavBar styles

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import nostalgia from "./images/nostalgia.png";
+import useStyles from "./styles";
 
 const App = () => {
+	const classes = useStyles();
+
 	return (
 		<Container maxWidth="lg">
-			<AppBar position="static" color="inherit">
-				<Typography variant="h2" align="center">
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography className={classes.heading} variant="h2" align="center">
 					Nostalgia
 				</Typography>
-				<img src={nostalgia} alt="nostalgia_icon" height="60" />
+				<img
+					className={classes.image}
+					src={nostalgia}
+					alt="nostalgia_icon"
+					height="60"
+				/>
 			</AppBar>
 			<Grow in>
 				<Container>
