92fbfb155a5f9500869254b39dc7046c4b36551c
vivekapadia <610vivek@gmail.com>
2022-04-10 17:31:12 +0530

    multiple like functionality added

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -13,7 +13,7 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
 
-import { deletePost } from "../../../actions/posts";
+import { likePost, deletePost } from "../../../actions/posts";
 import useStyles from "./styles";
 
 const Post = ({ post, setCurrentId }) => {
@@ -62,7 +62,11 @@ const Post = ({ post, setCurrentId }) => {
 				</Typography>
 			</CardContent>
 			<CardActions className={classes.cardActions}>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => dispatch(likePost(post._id))}
+				>
 					<ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}
 				</Button>
 				<Button
