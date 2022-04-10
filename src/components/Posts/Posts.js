ef46ba15384a5dc63b27fa7d4002004bff2d585f
vivekapadia <610vivek@gmail.com>
2022-04-10 10:46:50 +0530

    fetch the data for posts from global redux store

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Post from "./Post/Post.js";
 import useStyles from "./styles";
 
 const Posts = () => {
 	const classes = useStyles();
+	const posts = useSelector((state) => state.posts);
+
+	console.log("Posts.js: posts: ", posts);
+
 	return (
 		<>
 			<h1>Posts</h1>
