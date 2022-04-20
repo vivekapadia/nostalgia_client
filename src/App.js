8b3888c05268caee8955a6e9b0b2acb8fb0c8491
vivekapadia <610vivek@gmail.com>
2022-04-20 20:51:48 +0530

    Transfer GridPosts from appJs into new separate home component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Container} from "@material-ui/core";
 
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
 import Navbar from "./components/Navbar/Navbar";
-
-import useStyles from "./styles";
+import Home from "./components/Home/Home";
 
 const App = () => {
-	const [currentId, setCurrentId] = useState(null);
-	const classes = useStyles();
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(getPosts());
-	}, [dispatch]);
-
+	
 	return (
 		<Container maxWidth="lg">
 			<Navbar />
-			<Grow in>
-				<Container>
-					<Grid
-						className={classes.mainContainer}
-						container
-						justify="space-between"
-						alignItems="stretch"
-						spacing={3}
-					>
-						<Grid item sm={12} md={7}>
-							<Posts setCurrentId={setCurrentId} />
-						</Grid>
-						<Grid item sm={12} md={4}>
-							<Form currentId={currentId} setCurrentId={setCurrentId} />
-						</Grid>
-					</Grid>
-				</Container>
-			</Grow>
+			<Home />
 		</Container>
 	);
 };
---
+++ src/components/Home/Home.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "../../actions/posts";
+import Posts from "../../components/Posts/Posts";
+import Form from "../../components/Form/Form";
+
+import useStyles from "./styles";
+
+const Home = () => {
+	const [currentId, setCurrentId] = useState(null);
+	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getPosts());
+	}, [dispatch]);
+
+	return (
+		<Grow in>
+			<Container>
+				<Grid
+					className={classes.mainContainer}
+					container
+					justify="space-between"
+					alignItems="stretch"
+					spacing={3}
+				>
+					<Grid item sm={12} md={7}>
+						<Posts setCurrentId={setCurrentId} />
+					</Grid>
+					<Grid item sm={12} md={4}>
+						<Form currentId={currentId} setCurrentId={setCurrentId} />
+					</Grid>
+				</Grid>
+			</Container>
+		</Grow>
+	);
+};
+
+export default Home;
