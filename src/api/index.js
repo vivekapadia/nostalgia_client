52699dbba57faee75b95a1491638b936da8ff77d
vivekapadia <610vivek@gmail.com>
2022-04-24 15:46:33 +0530

    send token to backend

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,16 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 // heroku
 // const url = "https://nostalgiaishere.herokuapp.com/posts";
 
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem("profile")) {
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem("profile")).token
+		}`;
+	}
+
+	return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
